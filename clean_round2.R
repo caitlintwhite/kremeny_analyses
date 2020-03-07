f3ed8a3af3080f5fb006b4bcd0471f2b33ec5879
Caitlin White <caitlin.t.white@colorado.edu>
2020-03-06 17:20:35 -0700

    ongoing edits

--- clean_round2.R
+++ clean_round2.R
@@ -12,7 +12,7 @@ qualtrix <- read.csv(file.choose(), na.strings = na_vals, skip  = 2)
 headers <- read.csv(file.choose(), na.strings = na_vals)
 
 
-# -- DATA PREP -----
+# -- IDENTIFY DOUBLE-REVIEWED PAPERS -----
 # remove top two header rows
 names(qualtrix) <- names(headers)
 
@@ -29,13 +29,42 @@ prelim <- subset(qualtrix, Finished == "True" & Status != "Survey Preview" & !is
   filter(!doubleentry) %>%
   # remove doubleentryrow
   select(-doubleentry)
-
+# tally papers that have been reviewed more than once (i.e. 2 people reviewed it)
 records <- dplyr::select(prelim, Q27, Q1) %>%
   group_by(Q1) %>%
   mutate(nobs = length(Q1)) %>%
   ungroup()
-
+# write out double-reviewed for Aislyn to compare results 2020-03-04
 forAK <- subset(prelim, Q1 %in% unique(records$Q1[records$nobs == 2])) %>%
   arrange(Q1, StartDate) %>%
   mutate_all(function(x) ifelse(is.na(x), "", x))
-write.csv(forAK, "round2_doublereviewed.csv", row.names = F)
+#write.csv(forAK, "round2_doublereviewed.csv", row.names = F)
+
+# write out headers to manually create abbreviations for headers
+headerdf <- data.frame(headers[1,]) %>%
+  gather(id, fullquestion, StartDate:ncol(.))
+
+
+# -- DIVIDE RESULTS BY QUESTION ---
+names(prelim)
+# important ID-key fields to keep across question datasets are:
+## ResponseID
+## Q27 (Reviewer ID)
+## Q1 (Title)
+
+keydf <- distinct(dplyr::select(prelim, RecordedDate, ResponseId, Q27, Q1))
+# who has reviewed?
+sort(unique(keydf$Q27))
+# how many papers per reviewer?
+sort(sapply(split(keydf$Q1, keydf$Q27), length))
+
+# filtering (exclusion) questions are Q2, Q29, (and Q30? not sure what question that is)
+# pull unique question names
+questions <- names(prelim)[grep("^Q", names(prelim))]
+question_numbers <- unique(str_extract(questions, "^Q[:number:]+"))
+
+# question 9 (scale)
+q9df <- dplyr::select(prelim, c(names(keydf), names(prelim)[grep("Q25", names(prelim))]))
+# question 25 (response variables)
+
+
