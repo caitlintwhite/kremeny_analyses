88b328e479ec48d2441792e42921c7a7b6943c95
Caitlin White <caitlin.t.white@colorado.edu>
2020-03-08 23:26:43 -0600

    tidy round 2, make prelim fig on q12

--- clean_round2.R
+++ clean_round2.R
@@ -1,15 +1,22 @@
 # clean round 2 results
 
+# how to read in qualtrix dynamically
+# https://www.rdocumentation.org/packages/qualtRics/versions/3.0
 
 # -- SETUP -----
 library(tidyverse)
 library(lubridate)
+library(cowplot)
 options(stringsAsFactors = F)
 na_vals <- c("NA", NA, "NaN", NaN, " ", "", ".")
+theme_set(theme_bw())
 
 # find where the prelim results live
 qualtrix <- read.csv(file.choose(), na.strings = na_vals, skip  = 2) 
 headers <- read.csv(file.choose(), na.strings = na_vals)
+
+# read in header lookup table
+headerLUT <- read.csv("headersLUT.csv", na.strings = na_vals)
 
 
 # -- IDENTIFY DOUBLE-REVIEWED PAPERS -----
@@ -40,9 +47,39 @@ forAK <- subset(prelim, Q1 %in% unique(records$Q1[records$nobs == 2])) %>%
   mutate_all(function(x) ifelse(is.na(x), "", x))
 #write.csv(forAK, "round2_doublereviewed.csv", row.names = F)
 
+
+# -- PREP HEADER LOOKUP TABLE -----
 # write out headers to manually create abbreviations for headers
-headerdf <- data.frame(headers[1,]) %>%
-  gather(id, fullquestion, StartDate:ncol(.))
+# headerdf <- data.frame(headers[1,]) %>%
+#   gather(id, fullquestion, StartDate:ncol(.))
+# write_csv(headerdf, "headers.csv")
+## made header LUT from this basic data table, so only needed to run code once
+
+str(headerLUT)
+# append out question number to frame
+headerLUT$qnum <- str_extract(headerLUT$fullquestion, "Q[:number:]+")
+# what needs aq#?
+headerLUT$abbr[is.na(headerLUT$qnum)]
+# fill in/clean up any question that didn't have a Q#
+headerLUT$qnum[headerLUT$abbr %in% c("ReviewOnly", "GenInfo", "Uncertainty", "SurveyNotes")]  <- c("Q3", "Q6", "Q17", "Q18")
+# extract ES number for Q25 (for eventually joining ES abbreviation)
+## will be at position .x_x
+headerLUT$ESnum <- str_extract(headerLUT$id, "(?<=Q25.[:number:]_)[:number:]+(_)?") %>% parse_number()
+# extract ES number + abbreviation
+ESshort <- distinct(dplyr::select(headerLUT, ESnum, ES)) %>% na.omit()
+# redo group
+headerLUT <- headerLUT %>%
+  mutate(Group = ifelse(grepl("plot|site", abbr, ignore.case = T), gsub("[P|Sa-z]+_", "", headerLUT$abbr), 
+                        ifelse(grepl("Human", fullquestion) & qnum == "Q12", "Anthro",
+                               ifelse(grepl("Biotic", fullquestion) & qnum == "Q12", "Bio",
+                                      ifelse(grepl("Envir", fullquestion) & qnum == "Q12", "Env", NA))))) %>%
+  #drop ES column and join ESshort
+  dplyr::select(-ES) %>%
+  left_join(ESshort) %>%
+  # clean up abbr for scale question
+  mutate(abbr = gsub("_[1-9].*|_unk", "", abbr),
+         survey_order = parse_number(row.names(.)))
+
 
 
 # -- DIVIDE RESULTS BY QUESTION ---
@@ -62,9 +99,111 @@ sort(sapply(split(keydf$Q1, keydf$Q27), length))
 # pull unique question names
 questions <- names(prelim)[grep("^Q", names(prelim))]
 question_numbers <- unique(str_extract(questions, "^Q[:number:]+"))
-
+# goal is to create tidy data frames for analysis
 # question 9 (scale)
-q9df <- dplyr::select(prelim, c(names(keydf), names(prelim)[grep("Q25", names(prelim))]))
+q9df <- dplyr::select(prelim, c(names(keydf), names(prelim)[grep("Q9", names(prelim))]))
+
+
+# make tidy dataset
+prelimlong <- prelim %>%
+  dplyr::select(StartDate, EndDate, RecordedDate, Q27:ncol(.)) %>%
+  gather(id, answer, Q2:ncol(.)) %>%
+  left_join(headerLUT) %>%
+  arrange(RecordedDate) %>%
+  rename(Init = Q27, Title = Q1)
+
+# for prelim results, just look at single/first reviewed
+firstreview <- prelimlong %>%
+  group_by(Title, abbr, id) %>%
+  filter(!duplicated(Title)) %>%
+  ungroup()
+
+# exclusion questions
+ggplot(subset(firstreview, qnum == "Q3"), aes(abbr, fill = answer)) +
+  geom_bar()
+
+# ES question
 # question 25 (response variables)
+q25df <- subset(firstreview, qnum =="Q12") %>%
+  filter(!is.na(answer))
+
+select(q25df, Init, Title, ES) %>%
+  filter(!is.na(ES)) %>%
+  distinct() %>%
+  ggplot(aes(ES)) +
+  geom_bar() +
+  coord_flip()
+
+ytypefig <- select(q25df, Init, Title, ES, abbr, answer) %>%
+  filter(abbr == "Yclass") %>%
+  distinct() %>%
+  mutate(answer = gsub("Proxy for ES", "ES proxy", answer),
+         answer = factor(answer, levels = c("EF", "ES,EF", "ES", "ES,ES proxy", "ES proxy"))) %>%
+  ggplot(aes(ES, fill = answer)) +
+  geom_bar(color = "grey30") +
+  scale_y_continuous(expand = c(0,0)) +
+  scale_fill_viridis_d(name = "Response\ntype") +
+  facet_wrap(~"") +
+  theme(strip.background = element_blank(),
+        strip.text = element_text(face = "bold"),
+        legend.position = "bottom",
+        legend.justification = c(-1,-1),
+        legend.title = element_text(size = 10)) +
+  guides(fill = guide_legend(nrow = 2)) +
+  coord_flip()
+ytypefig
+
+test <- select(q25df, Init, Title, ES, abbr, Group, answer) %>%
+  # for now fill down any NAs with ES above it, grouped by Title
+  group_by(Title) %>%
+  fill(ES) %>%
+  ungroup() %>%
+  filter(grepl("Drive", abbr)) %>%
+  distinct() %>%
+  # remove other from Driver (assume if there was something people answered)
+  mutate(count_driver = str_count(answer, "(,|;)(?!Other)"),
+         #if Exploitation, subtract 1 bc has comma in answer
+         #count_driver = ifelse(grepl("Exploitation [(]hunt", answer), count_driver-1, count_driver),
+         count_driver = ifelse(!grepl("Other|Exploit", answer), count_driver+1, count_driver)) %>%
+  select(-answer) %>%
+  group_by(Init, Title, ES, Group) %>%
+  summarise(CountDrivers = sum(count_driver, na.rm = T)) %>% #IDK if we can use other...
+  ungroup()
+  # count the number of commas in each -- assume there is 1 answer provided if not NA
+  
+ggplot(test, aes(ES, fill = Group)) +
+  geom_bar() +
+  coord_flip()
+
+driversfig <- test %>%
+  mutate(CountDrivers = ifelse(CountDrivers == 0, 1, CountDrivers)) %>%
+  group_by(Init, Title, ES, Group) %>%
+  summarise(numberDrivers = sum(CountDrivers)) %>%
+  ungroup()  %>%
+  ggplot(aes(ES, fill = as.factor(numberDrivers))) +
+  geom_bar(color = "grey30") +
+  scale_y_continuous(expand = c(0,0)) +
+  scale_fill_brewer(name = "Number of\ndrivers", palette = "Blues") +
+  facet_wrap(~Group) +
+  theme(strip.background = element_blank(),
+        strip.text = element_text(face = "bold"),
+        legend.position = "bottom",
+        axis.text.y = element_blank(),
+        axis.title.y = element_blank(),
+        legend.title = element_text(size = 10)) +
+  guides(guide_legend(byrow = T)) +
+  coord_flip()
+
+prelimfig <- plot_grid(ytypefig, driversfig, nrow = 1, labels = "AUTO")
+ggsave("round2_prelimfig.pdf", prelimfig, 
+       width = 8, height = 5, units = "in", scale = 1.1)  
 
 
+# -- DOUBLE REVIEWED ----
+doubleprelim <- subset(prelimlong, Title %in% records$Q1[records$nobs == 2]) %>%
+  group_by(Title, id) %>%
+  mutate(same_answer = length(unique(answer)) ==1) %>%
+  ungroup() %>%
+  filter(!(same_answer & is.na(answer))) %>%
+  arrange(Title, survey_order, RecordedDate)
+write_csv(doubleprelim, "round2_doublereviewed_tidy.csv")
