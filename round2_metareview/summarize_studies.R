c9a0f43df41c7f6d9afe9b47201f920ab89b2e31
Caitlin White <caitlin.t.white@colorado.edu>
2020-04-09 14:40:15 -0600

    all basic set up except temp, wip

--- round2_metareview/summarize_studies.R
+++ round2_metareview/summarize_studies.R
@@ -24,19 +24,16 @@ options(stringsAsFactors = F)
 na_vals <- c("NA", "NaN", "", " ", ".", NA, NaN)
 
 # read in latest metareview data
-dat <- read.csv("round2_metareview/data/cleaned/prelim_singlereview.csv", na.strings = na_vals)
+rawdat <- read.csv("round2_metareview/data/cleaned/prelim_singlereview.csv", na.strings = na_vals)
 # check read in as expected
-glimpse(dat)
-
-# for data susbetting reference, here are the Q#s and their abbreviations:
-distinct(dat[c("abbr", "qnum")])
+glimpse(rawdat)
 
 
 # function for breaking out comma separated answers
 splitcom <- function(df){
   df <- df %>% # break out answers by comma
     dplyr::select(Title, answer) %>%
-    separate(answer, paste0("v",1:20)) %>% # <- this will throw warning, fine. pad with extra cols for up to 20 comma-separated answers just in case
+    separate(answer, paste0("v",1:20), ",") %>% # <- this will throw warning, fine. pad with extra cols for up to 20 comma-separated answers just in case
     # remove cols that are all na
     dplyr::select(names(.)[sapply(., function(x) !(all(is.na(x))))]) %>%
     # gather to tidy
@@ -50,6 +47,20 @@ splitcom <- function(df){
 
 
 
+# -- PREP DATA -----
+# remove any titles that were excluded (yes in q3)
+excludetitles <- with(rawdat, Title[(qnum == "Q3" & grepl("yes", answer, ignore.case = T))]) 
+checkexcluded <- subset(rawdat, Title %in% excludetitles) # looks good
+rm(checkexcluded)
+
+# remove excluded papers
+dat <- subset(rawdat, !Title %in% excludetitles)
+
+# for data susbetting reference, here are the Q#s and their abbreviations:
+distinct(dat[c("abbr", "qnum")])
+
+
+
 # -- SUMMARIZE REVIEW DATA BY QUESTION -----
 # 1) Plot Ecosystem Type -----
 #table(krem$Q4)
@@ -58,14 +69,14 @@ View(ecotype)
 unique(ecotype$answer[ecotype$abbr == "Ecosytem"]) #ctw will fix typo later
 # for simplicity, keep only ecosytem checked (ignore notes for now)
 ecotype <- subset(ecotype, abbr == "Ecosytem") %>%
-  # edit "Off-shore" to avoid punctuation issues
-  mutate(answer = gsub("Off-shore", "Offshore", answer)) %>%
   # apply splitcom
   splitcom() # end result is still tidy data
 
 # simple bar plot
 ggplot(ecotype, aes(answer)) +
   geom_bar()
+table(ecotype$answer)
+table(ecotype$num) # most papers study only one type of system, but multiple systems entered for +100 papers (about 30% of papers reviewed)
 
 
 
@@ -77,6 +88,8 @@ loc <- splitcom(loc)
 # simple bar plot
 ggplot(loc, aes(answer)) +
   geom_bar()
+table(loc$answer)
+table(loc$num)
 
 
 
@@ -93,7 +106,8 @@ studytype <- subset(studytype, abbr == "Methods") %>%
 # simple bar plot
 ggplot(studytype, aes(answer)) +
   geom_bar()
-
+table(studytype$answer)
+table(studytype$num)
 
 
 # 3) Consideration of multiple years & # of years -----
@@ -107,9 +121,29 @@ unique(temptype$abbr)
 # 4) Plot Feedbacks counts -----
 #table(krem$Q21)
 feedbacks <- subset(dat, abbr == "Feedbacks")
+unique(feedbacks2$answer) # needs comma splitting
+feedbacks <- splitcom(feedbacks)
+
+# simple bar plot
+ggplot(feedbacks, aes(answer)) +
+  geom_bar() +
+  # hard to see when on answers on x
+  coord_flip()
+table(feedbacks$answer)
+table(feedbacks$num)
 
 
 
 # 5) Plot Threshold/Non-linearity counts -----
 #table(krem$Q22)
 tholds <- subset(dat, abbr == "Thresholds")
+unique(tholds$answer) # no un-comma needed
+unique(tholds$abbr) # no subquestions or optional text comments to this question
+
+# simple bar plot
+ggplot(tholds, aes(answer)) +
+  geom_bar()
+table(tholds$answer)
+  
+
+
