8795cbc2442524caae3b6492782e280f278e6c74
Caitlin White <caitlin.t.white@colorado.edu>
2020-06-03 21:34:06 -0600

    remove old code

--- round2_metareview/analyze_data/make_q12ESmatrix_figs.R
+++ round2_metareview/analyze_data/make_q12ESmatrix_figs.R
@@ -47,7 +47,7 @@ theme_set(theme_classic())
 
 
 # read in latest metareview data
-rawdat <- read.csv("round2_metareview/data/cleaned/prelim_singlereview.csv", na.strings = na_vals)
+rawdat <- read.csv("round2_metareview/data/intermediate/prelim_singlereview.csv", na.strings = na_vals)
 # check read in as expected
 glimpse(rawdat)
 
@@ -71,53 +71,6 @@ splitcom <- function(df, keepcols = c("Title", "answer")){
   return(df)
 }
 
-# for reference, ctw code for paneling ggplots in single window
-# function to panel plot flagged data
-#visual_qa <- function(dat, qadat, sorttime = "date", add_fourth = NA){
-  # initiate list for storing ggplots
-  plot_list <- list()
-  # id temperature cols in reference data frame
-  tempcols <- colnames(dat)[grepl("temp", colnames(dat))]
-  
-  for(m in c("airtemp_max", "airtemp_min")){
-    tempdf <- qadat[qadat$met == m,] %>% as.data.frame()
-    # order by preferred time sort (default is date)
-    tempdf <- tempdf[order( tempdf[,which(colnames(tempdf) == sorttime)]),]
-    
-    for(d in as.character(tempdf$date)){
-      d <- as.Date(d, format = "%Y-%m-%d")
-      tempplot <- ggplot(data = subset(dat, met == m & date %in% seq(as.Date(d)-10, as.Date(d)+10, 1))) +
-        geom_line(aes(date, main)) +
-        geom_point(aes(date, main)) +
-        # circle the flagged value in red
-        geom_point(data = subset(dat, met == m & date == as.Date(d)),
-                   aes(date, main), col = "red", pch  = 1, size = 3) +
-        labs(y = gsub("airtemp_", "T", m),
-             x = d) +
-        # add sdl chart temp for comparison (purple dots)
-        geom_line(aes(date, comp1), col = "purple") +
-        geom_point(aes(date, comp1), col = "purple", pch = 1) +
-        geom_line(aes(date, comp2), col = "steelblue2") +
-        geom_point(aes(date, comp2), col = "steelblue4", pch = 1) +
-        geom_line(aes(date, comp3), col = "forestgreen") +
-        geom_point(aes(date, comp3), col = "darkgreen", pch = 1) +
-        theme_bw()
-      
-      if(!is.na(add_fourth)){
-        colnames(dat)[colnames(dat) == add_fourth] <- "comp4"
-        tempplot <- tempplot + 
-          geom_line(data = subset(dat, met == m & date %in% seq(as.Date(d)-10, as.Date(d)+10, 1)), 
-                    aes(date, comp4), col = "goldenrod1") + 
-          geom_point(data = subset(dat, met == m & date %in% seq(as.Date(d)-10, as.Date(d)+10, 1)), 
-                     aes(date, comp4), col = "goldenrod1", pch = 1)
-      }
-      
-      # store plot in a list
-      plot_list[length(plot_list)+1] <- list(tempplot)
-    }
-  }
-  return(plot_list)
-}
 
 
 
@@ -265,32 +218,6 @@ title(sub = "Soil Protection ES/EF")
 dev.off()
 
 
-
-
-#par(mfrow = c(4,4))
-#for(v in unique(response_summary_simple$ES)){
-  tempdat <- subset(response_summary_simple, ES == v)
-  wordcloud(words = tempdat$yresponse, freq = tempdat$count, main = v, 
-            min.freq = 1, scale = c(2, 0.4), max.words=200, random.order=FALSE, rot.per=0.35,
-            colors=brewer.pal(8, "Dark2"))
-  title(sub = v)
-}
-
-#plot_clouds <- function(dat, splitvar){
-  # initiate list for storing ggplots
-  plot_list <- list()
-  
-  for(v in splitvar){
-    tempdat <- subset(dat, ES == v)
-    tempplot <- wordcloud(words = tempdat$yresponse, freq = tempdat$count, main = v, 
-                          min.freq = 1, scale = c(2, 0.4), max.words=200, random.order=FALSE, rot.per=0.35,
-                          colors=brewer.pal(8, "Dark2"))
-    # store plot in a list
-    plot_list[length(plot_list)+1] <- list(tempplot)
-  }
-  return(plot_list)
-  
-}
 
 
 
