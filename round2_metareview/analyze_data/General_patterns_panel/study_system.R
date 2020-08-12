2865f50264c3692d1f2e4c57cc378e30c088be33
Grant Vagle <grant.vagle@colorado.edu>
2020-08-12 10:18:31 -0600

    simple version of study system fig

--- round2_metareview/analyze_data/General_patterns_panel/study_system.R
+++ round2_metareview/analyze_data/General_patterns_panel/study_system.R
@@ -32,7 +32,31 @@ ggsave('round2_metareview/analyze_data/General_patterns_panel/fig_files/study_sy
 
 
 
+# simplifying the subgroups
 
+# some 'Urban' without terrestrial (and without anything else either) that should get changed to Terrestrial
+# None are only 'Agriculture/Agroforestry/Rural'
+# None are 'Agriculture/Agroforestry/Rural' without terrestrial or wetland/riparian
+
+# for now: rely on reported study systems, filter to terrestrial, marine, coastal, freshwater
+  # the only issues are Urban studies without any other type listed, and what to do with wetland/riparian
+
+dat %>% 
+  filter(abbr == 'Ecosystem') %>%
+  dplyr::select(Title, clean_answer) %>% 
+  separate_rows(clean_answer, sep = ',') %>%
+  filter(clean_answer %in% c('Terrestrial','Marine/Off-shore','Coastal','Freshwater')) %>%
+  group_by(clean_answer) %>%
+  summarise(count = n()) %>% 
+  mutate(proportion = count / num_papers) %>%
+  ggplot(aes(x = fct_reorder(clean_answer, proportion), y = proportion, label = round(proportion, digits = 2))) +
+  geom_col() +
+  xlab('Study system') +
+  ylab('Proportion of studies') +
+  coord_flip() +
+  theme_bw() 
+
+ggsave('round2_metareview/analyze_data/General_patterns_panel/fig_files/study_system_simple.pdf', width = 5, height = 5, dpi = 'retina')
 
 
 
