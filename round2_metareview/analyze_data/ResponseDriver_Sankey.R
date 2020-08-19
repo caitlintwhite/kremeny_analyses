5f0fd90fee96652cdf3942bacbdc62a43b98a0c2
Aislyn <aike2863@colorado.edu>
2020-08-19 13:28:05 -0600

    Updated Sankey, will add labels and legend in Pwpt now!

--- round2_metareview/analyze_data/ResponseDriver_Sankey.R
+++ round2_metareview/analyze_data/ResponseDriver_Sankey.R
@@ -16,7 +16,7 @@ library(htmlwidgets)
 data <- read.csv("round2_metareview/data/cleaned/ESqualtrics_r2keep_cleaned.csv")
 
 data <- data[data$version == "final",] # final data
-df <- data[,c(14,16:18)] # pull relevant columns
+df <- data[,c(15:19)] # pull relevant columns
 df <- df[df$qnum == "Q12",] # subset to only Q12
 
 
@@ -42,8 +42,9 @@ attach(nodes)
 nodes <- nodes[order(NodeType,name),]
 detach(nodes)
 
+nodes <- nodes[c(3,1,2,4:19),]
 
-nodes$NodeType <- c("Anthro","Bio","Env", "Regulating", "Regulating","Regulating",
+nodes$NodeType <- c("Human","Biotic","Environmental", "Regulating", "Regulating","Regulating",
                     "Cultural","Provisioning","Provisioning","Regulating","Supporting",
                     "Regulating","Cultural","Provisioning","Provisioning","Regulating",
                     "Regulating","Supporting","Supporting")
@@ -57,7 +58,7 @@ detach(nodes)
 
 nodes <- nodes[c(1,2,3,10:16,17:19,6:9,4,5),] # reorder for sorting purposes on the actual plot
 
-nodes$FullName <- c("Anthropogenic", "Biotic", "Environmental",
+nodes$FullName <- c("Human", "Biotic", "Environmental",
                     "Air Quality","Climate Regulation","Coastal Water Quality",
                     "Fresh Water Quality","Hazard Regulation","Regulation of Ocean Acidification",
                     "Regulation of pests/pathogens","Habitat Creation","Pollination",
@@ -72,19 +73,26 @@ nodes$ID <- 0:x
 
 colnames(nodes)[colnames(nodes)=="name"] <- "TargetName"
 
+nodes[1,c(1,2)] <- "Human"
+nodes[2,c(1,2)] <- "Biotic"
+nodes[3,c(1,2)] <- "Environmental"
+
+nodes$FullName[1:3] <- ""
+
 ## Link data frame for sankey
 # link VALUES
-values <- driver.df[,c(2,3,4)]
+values <- driver.df[,c(2,3,5)]
 values$num.papers <- 1 # add column to sum
 values <- aggregate(formula = num.papers ~ ES + clean_group , data = values, FUN = sum)
 col <- c("TargetName","SourceName","value")
 colnames(values) <- col
 
 
+
 # create data frame to merge later
-links <- data.frame(SourceName = rep(c("Bio","Anthro","Env"), each=nrow(nodes[nodes$Type=="eco.serv",])),
-                    Source = rep(c(1,0,2), each=nrow(nodes[nodes$Type=="eco.serv",])),
-                    TargetName = rep(nodes$name[c(4:19)], times = 3),
+links <- data.frame(SourceName = rep(c("Human","Biotic","Environmental"), each=nrow(nodes[nodes$Type=="eco.serv",])),
+                    Source = rep(c(0,1,2), each=nrow(nodes[nodes$Type=="eco.serv",])),
+                    TargetName = rep(nodes$TargetName[c(4:19)], times = 3),
                     Target = rep(nodes$ID[4:nrow(nodes)], times=3)
 ) 
 
@@ -98,18 +106,20 @@ names(s.links)[names(s.links)=="x"] <- "value"
 s.links <- merge(s.links,nodes,by="TargetName", all.x=T)
 
 attach(s.links)
-s.links <- s.links[order(TargetName,NodeType),]
+s.links <- s.links[order(NodeType, value),]
 detach(s.links)
 
-colors <-  'd3.scaleOrdinal() .domain(["Anthro", "Bio","Env", "Regulating","Supporting","Provisioning","Cultural"]) 
+colors <-  'd3.scaleOrdinal() .domain(["Human", "Biotic","Environmental", "Regulating","Supporting","Provisioning","Cultural"]) 
 .range(["#ADD8E6", "#117733","#00008B","#000000","#808080","#D3D3D3","#FFFFFF"])'
 
 
 
 # BASE SANKEY CODE
+
+
 sn <- sankeyNetwork(Links = s.links, Nodes = nodes, Source = "Source",
               Target = "Target", Value = "value", NodeID = "FullName",
-              units = "papers", fontSize = 20, nodeWidth = 30,
+              units = "papers", fontSize = 17, nodeWidth = 30,
               fontFamily = "Arial",
               LinkGroup = "SourceName",
               NodeGroup = "NodeType",
