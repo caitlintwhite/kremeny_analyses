e98bcadae5eae802a5c14eeef43ae8fdd9b4d31d
Grant Vagle <grant.vagle@colorado.edu>
2021-06-30 11:38:27 -0600

    add 'other' bar to biotic drivers plot

--- round2_metareview/analyze_data/Drivers_nonsankey/drivers_types_within_groups.R
+++ round2_metareview/analyze_data/Drivers_nonsankey/drivers_types_within_groups.R
@@ -19,17 +19,30 @@ drivers_binned = dat %>%
   summarise(count = n()) 
 
 # biotic
+biotic_other_prop = 1 - drivers_binned %>%
+  filter(Group == 'Biotic') %>%
+  mutate(proportion = count / sum(count)) %>%
+  filter(proportion > 0.06) %>%
+  pull(proportion) %>%
+  sum()
+
+
+biotic_other_row = data.frame('Group'='Biotic', clean_answer_binned = 'Other biotic driver', count = NA, proportion = biotic_other_prop)
+
 drivers_binned %>%
   filter(Group == 'Biotic') %>%
   mutate(proportion = count / sum(count)) %>%
   filter(proportion > 0.06) %>% 
-  ggplot(aes(x = fct_reorder(clean_answer_binned, proportion), y = proportion)) +
+  rbind(biotic_other_row) %>%
+  mutate(clean_answer_binned = factor(clean_answer_binned, levels = c('Service provider identity', 'Service provider diversity/richness','Biotic characteristics of the plot', 'Other biotic driver'))) %>%
+  ggplot(aes(x = fct_rev(clean_answer_binned), y = proportion)) +
   geom_col() +
   xlab('') +
   ylab('Proportion of biotic drivers') +
   ggtitle('Biotic drivers') +
   coord_flip() +
   theme_bw()
+
 ggsave('round2_metareview/analyze_data/Drivers_nonsankey/fig_files/biotic_binned.pdf', width = 5, height = 3, dpi = 'retina')
 
 # human
